cc8d49de72443f09c988226d06086f9d9c27a3f6
osamaalhulays <osamaalhulays@gmail.com>
2025-08-11 20:48:49 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverActions: false },
   reactStrictMode: true,
   poweredByHeader: false,
-  headers: async () => [
-    { source: "/(.*)", headers: [
-      { key: "Cache-Control", value: "no-store" },
-      { key: "X-Content-Type-Options", value: "nosniff" }
-    ]}
-  ]
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Cache-Control", value: "no-store" },
+          { key: "X-Content-Type-Options", value: "nosniff" }
+        ]
+      }
+    ];
+  }
 };
 export default nextConfig;
