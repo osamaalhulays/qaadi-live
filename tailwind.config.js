5ba73cfed01168ffdd8bac87f3ddf98dd385b725
osamaalhulays <osamaalhulays@gmail.com>
2025-08-14 14:54:27 +0300

    chore: configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1e40af",
+        secondary: "#f59e0b"
+      }
+    }
+  },
+  plugins: []
+};
