1c21385609b7f3eaff11384e816d4fcc5b2f7fae
osamaalhulays <osamaalhulays@gmail.com>
2025-08-18 16:08:23 +0300

    chore: expose QN21 criterion interface

--- src/lib/workflow/index.ts
+++ src/lib/workflow/index.ts
@@ -1,4 +1,8 @@
 export { runGates } from "./gates";
 export type { GateResult, SecretaryReport } from "./gates";
 export { gateQn21 } from "./qnGate";
-export type { QN21GateResult, QN21Report } from "./qnGate";
+export type {
+  QN21GateResult,
+  QN21Report,
+  QN21Criterion,
+} from "./qnGate";
