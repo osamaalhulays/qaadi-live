b7d1507b89e6a3da8672351602fa9544fa56f3a1
osamaalhulays <osamaalhulays@gmail.com>
2025-08-20 22:09:15 +0300

    refactor test assertions

--- test/generationService.test.ts
+++ test/generationService.test.ts
@@ -8,7 +8,10 @@ test('generateText delegates to provided runner', async () => {
   const res = await generateText('auto', keys, 'prompt', 5, runner);
   assert.deepStrictEqual(res, { text: 'hi' });
   assert.strictEqual(runner.mock.calls.length, 1);
-  assert.deepStrictEqual(runner.mock.calls[0], ['auto', {}, 'prompt', 5]);
+  assert.strictEqual(runner.mock.calls[0][0], 'auto');
+  assert.deepStrictEqual(runner.mock.calls[0][1], {});
+  assert.strictEqual(runner.mock.calls[0][2], 'prompt');
+  assert.strictEqual(runner.mock.calls[0][3], 5);
   assert.strictEqual(
     Object.getPrototypeOf(runner.mock.calls[0][1]),
     Object.prototype
